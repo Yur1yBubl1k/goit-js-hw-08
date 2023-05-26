567e3bef5d75bfa5eba6ebbe0c7776fa0d28d18e
Bubl1k <122160505+Yur1yBubl1k@users.noreply.github.com>
2023-05-27 01:14:35 +0300

    testCheck

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,44 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
 
-console.log(galleryItems);
+
+// console.log(galleryItems);
+
+
+const ulListElemSecond = document.querySelector('.gallery');
+// console.log(ulListElemSecond);
+
+const createImageListFuncSecond = galleryItems.map(({preview, original, description}) =>{
+    return `
+    <li class="gallery__item">
+   <a class="gallery__link" href="${original}">
+      <img class="gallery__image" 
+      src="${preview}" 
+      alt="${description}" />
+   </a>
+</li>`
+}).join('')
+
+ulListElemSecond.insertAdjacentHTML("beforeend",createImageListFuncSecond);
+
+let gallery = new SimpleLightbox('.gallery a', {
+    captionsData: 'alt', 
+    captionDelay: 250,
+    captionPosition: 'bottom',
+    close: true,
+});
+
+// const clickFunc = (event) => {
+//     event.preventDefault();
+  
+//   const isCardReview =event.target.classList.contains("gallery__image");
+//   if( !isCardReview) {
+//       return;
+//   } 
+//   console.log(event.target.src);
+//   };
+  
+//   ulListElemSecond.addEventListener("click", clickFunc);
